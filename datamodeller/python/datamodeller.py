69da1fb279390b594faee578e43becaaf09e01f9
itsrag@gmail.com <itsrag@gmail.com>
2013-07-20 09:57:35 -0400

    Adding files for bloomberg contest

--- datamodeller/python/datamodeller.py
+++ datamodeller/python/datamodeller.py
@@ -6,12 +6,26 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support,classification_report
 from sklearn import preprocessing as pp
 from sklearn import cross_validation as cv
-from sklearn.decomposition import SparsePCA
+from sklearn.decomposition import SparsePCA,PCA
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.neighbors import KNeighborsClassifier
 import os
 from sklearn import svm, grid_search
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.cluster import KMeans
 
+def pearsonr(x, y):
+  # Assume len(x) == len(y)
+  n = len(x)
+  sum_x = float(sum(x))
+  sum_y = float(sum(y))
+  sum_x_sq = sum(map(lambda x: pow(x, 2), x))
+  sum_y_sq = sum(map(lambda x: pow(x, 2), y))
+  psum = sum(imap(lambda x, y: x * y, x, y))
+  num = psum - (sum_x * sum_y/n)
+  den = pow((sum_x_sq - pow(sum_x, 2) / n) * (sum_y_sq - pow(sum_y, 2) / n), 0.5)
+  if den == 0: return 0
+  return num / den
 
 def printPrecRecall(scores):
     appendDataTofile("Class Label        -1           0               1                2")
@@ -22,10 +36,18 @@ def printPrecRecall(scores):
     appendDataTofile("\n")    
     
 def transform(xTrain,yTrain,xTest):
-    pca = SparsePCA(n_components=200,n_jobs=10);
+    pca = SparsePCA(n_components=2);
     newXTrain =  pca.fit_transform(xTrain,yTrain)
     newXTest = pca.transform(xTest)
     return newXTrain,newXTest   
+
+def evaluateCorrelationResults(xTrain,yTrain):
+    outputFile = open("../files/correlation.csv", 'w+')
+    for i in range(1,xTrain.shape[1]):
+        correlation = scipy.stats.pearsonr(xTrain[:,i],yTrain)
+        outputFile.write("i = "+str(i)+" pearson correlation = "+str(correlation)+"\n")
+    outputFile.close()             
+
 
 def classify(func,xTrain,xTest,yTrain,yTest):        
         clf = func()
@@ -37,16 +59,24 @@ def classify(func,xTrain,xTest,yTrain,yTest):
         precScores = precision_recall_fscore_support(yTest, yPred);
         printPrecRecall(precScores)   
         return yPred   
+
+def clusterify(func,xTrain,xTest,yTrain,yTest):        
+        clf = func()
+        clf.fit(xTrain, yTrain);
+        yPred = clf.fit_predict(xTest); 
+        return yPred   
     
 def gridSearchCVforSVM(xTrain,xTest,yTrain,yTest):
      appendDataTofile("Grid search results")
      svc = svm.SVC()
-     parameters = {'kernel':('linear','rbf'), 'C':[1, 10]}
+     #parameters = {'kernel':('linear','rbf'), 'C':[1, 10]}
+     parameters = {'kernel':('linear','rbf','poly'), 'C':[1, 50],'gamma':[0.00,10.0]}
      clf = grid_search.GridSearchCV(svc,parameters)
      clf.fit(xTrain, yTrain)
      resultLR =  clf.score(xTest,yTest);  
-     appendDataTofile("accuracy =" , resultLR.mean());              
-     return resultLR   
+     appendDataTofile("accuracy =" , resultLR.mean());
+     pred =clf.predict(xTest);               
+     return pred   
       
     
 def appendDataTofile(*text_str):    
@@ -76,6 +106,10 @@ class DataModeller:
         xTest = testData[:, :testData.shape[1] -1]
         yTest = testData[:, testData.shape[1]-1]
         
+        #evaluateCorrelationResults(xTrain, yTrain)
+        
+        #xTrain,xTest = transform(xTrain,yTrain,xTest)
+        
         #tf-idf transformation
         transformer = TfidfTransformer()
         xTrain = transformer.fit_transform(xTrain)        
@@ -84,31 +118,38 @@ class DataModeller:
         appendDataTofile("Training dimension -> ",xTrain.shape)
         appendDataTofile("Testing dimension ->  ",xTest.shape)
         
-        #xTrain,xTest = transform(xTrain,yTrain,xTest)
         
+        
+                
         #MultinomialNB classification
-        appendDataTofile("MultiNB");
-        yPred = classify(lambda:naive_bayes.MultinomialNB(),xTrain,xTest,yTrain,yTest)
+        """appendDataTofile("MultiNB");
+        yPred = classify(lambda:naive_bayes.MultinomialNB(),xTrain,xTest,yTrain,yTest)"""
         
         #Logistic Regression classification
-        appendDataTofile("Log regression");
-        yPred = classify(lambda:linear_model.LogisticRegression(penalty="l1",C=0.5,intercept_scaling=2),
-                 xTrain,xTest,yTrain,yTest)
+        #penalty="l1",C=0.5,intercept_scaling=2
+        """appendDataTofile("Log regression");
+        yPred = classify(lambda:linear_model.LogisticRegression(),
+                 xTrain,xTest,yTrain,yTest)"""
                                   
-        #SVM based classification
+        """#SVM based classification
         appendDataTofile("SVM");
-        yPred = classify(lambda:svm.SVC(C=8.0,gamma=0.10,kernel='rbf',probability=True,shrinking=True),
-                 xTrain,xTest,yTrain,yTest)
+        #C=8.0,gamma=0.10,kernel='rbf',probability=True,shrinking=True
+        yPred = classify(lambda:svm.SVC(),
+                 xTrain,xTest,yTrain,yTest)"""
         
         #Grid search SVM
-        gridSearchCVforSVM(xTrain,xTest,yTrain,yTest)
- 
+        """yPred =  gridSearchCVforSVM(xTrain,xTest,yTrain,yTest)"""
         
-        outputFile = open("../files/classifiedtweets.csv", 'w+')
+        yPred =  clusterify(lambda:KMeans(n_clusters=3),xTrain,xTest,yTrain,yTest)
+         
+        """yPred = classify(lambda:KNeighborsClassifier(),xTrain,xTest,yTrain,yTest)""" 
+        """yPred = classify(lambda: linear_model.RidgeClassifierCV,xTrain,xTest,yTrain,yTest)"""
+        
+        outputFile = open("/Users/raghavan/Documents/workspace/datamining/data/raghavan/classificationresults.txt", 'w+')
         rows = len(yPred)
-        outputFile.write("Predicted,Actual\n");
+        #outputFile.write("ID\tSentiment\n");
         for i in range(0,rows):
-            outputFile.write(str(yPred[i]) +","+ str(yTest[i])+"\n")
+            outputFile.write(str(yPred[i])+"\n")
         outputFile.close()
         
         
